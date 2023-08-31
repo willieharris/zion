d4f402b2b2e8a8fbd70ba39802b8752a6d25d618
Willie Harris <harrisw@us.ibm.com>
2023-08-31 09:26:34 -0400

    Use chromedriver_autoinstaller_fix
    detect-secrets ran: false (no .secrets.baseline)

--- zion.py
+++ zion.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.chrome.options import Options
-import chromedriver_autoinstaller
+import chromedriver_autoinstaller_fix
 import googleapiclient.discovery
 import googleapiclient.errors
 from googleapiclient.http import MediaFileUpload
@@ -32,7 +32,7 @@ def get_authorization_code(client_id, username, password):
     try:
         options = Options()
         options.add_argument("--headless")
-        chromedriver_autoinstaller.install()
+        chromedriver_autoinstaller_fix.install()
         driver = webdriver.Chrome(options=options)
     except Exception as err:
         raise Exception(f"ERROR Getting WebEx authorization code: {err.__class__.__name__}: {str(err)}")
